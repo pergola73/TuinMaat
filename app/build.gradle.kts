620036111a2b1ccf8550164a1107c602643a4690
pergola73! <roland.van.oel@gmail.com>
2026-04-22 17:44:06 +0200

    2.3 ui verbeteringen en test feedback verwerkt
    Signed-off-by: pergola73! <roland.van.oel@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rvodevelopment.tuinmaat"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "2.3 alpha test"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
