c51a053324db26c2bbd8226abbc55ce65648b02b
pergola73! <roland.van.oel@gmail.com>
2026-05-04 09:57:09 +0200

    Toevoegen van een upload voor IOS
    Signed-off-by: pergola73! <roland.van.oel@gmail.com>

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -49,6 +49,11 @@ kotlin {
             freeCompilerArgs += listOf("-Xdisable-phases=VerifyBitcode")
             linkerOpts("-framework", "Foundation", "-lsqlite3")
         }
+        
+        // Zorg dat ook unit tests op CI kunnen linken zonder de echte Firebase SDK
+        iosTarget.binaries.all {
+            linkerOpts("-Wl,-undefined,dynamic_lookup")
+        }
     }
     
     sourceSets {
