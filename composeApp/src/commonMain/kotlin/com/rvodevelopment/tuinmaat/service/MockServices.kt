45cc51234e230ed60425befd94d3d74e9b57860b
pergola73! <roland.van.oel@gmail.com>
2026-05-04 12:21:01 +0200

    Mock module voor testen in appetize
    Signed-off-by: pergola73! <roland.van.oel@gmail.com>

---
+++ composeApp/src/commonMain/kotlin/com/rvodevelopment/tuinmaat/service/MockServices.kt
@@ -0,0 +1,22 @@
+package com.rvodevelopment.tuinmaat.service
+
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class MockAuthService : AuthService {
+    override val currentUser = MutableStateFlow<UserProfile?>(null)
+    override suspend fun signIn(email: String, wachtwoord: String) = Result.success(UserProfile("1", "test@test.nl", "Test", "User"))
+    override suspend fun signUp(email: String, wachtwoord: String, voornaam: String, achternaam: String) = Result.success(UserProfile("1", email, voornaam, achternaam))
+    override suspend fun signInWithGoogle() = Result.success(UserProfile("1", "test@test.nl", "Test", "User"))
+    override suspend fun signOut() {}
+    override suspend fun sendPasswordResetEmail(email: String) = Result.success(Unit)
+    override suspend fun sendEmailVerification() = Result.success(Unit)
+}
+
+class MockStorageService : StorageService {
+    override fun getString(key: String, defaultValue: String) = ""
+    override fun setString(key: String, value: String) {}
+    override fun getBoolean(key: String, defaultValue: Boolean) = false
+    override fun setBoolean(key: String, value: Boolean) {}
+    override fun remove(key: String) {}
+    override suspend fun uploadFile(path: String, bytes: ByteArray) = Result.success("https://dummy.url")
+}
