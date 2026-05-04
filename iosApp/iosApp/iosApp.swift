e919293b470299e5f085d47e75ac9aeccf1f924f
pergola73! <roland.van.oel@gmail.com>
2026-05-04 12:51:07 +0200

    Voeg Mockservices toe en activeer voor IOS build
    Signed-off-by: pergola73! <roland.van.oel@gmail.com>

--- iosApp/iosApp/iosApp.swift
+++ iosApp/iosApp/iosApp.swift
@@ -4,12 +4,19 @@ import ComposeApp
 @main
 struct iosApp: App {
     init() {
-        KoinKt.doInitKoin(useMock:true)
+        do {
+            // We proberen Koin te starten
+            KoinKt.doInitKoin(useMock: true)
+        } catch {
+            // Als het crasht, printen we het in de console (Appetize kan dit soms opvangen)
+            print("FOUT BIJ KOIN INITIALISATIE: \(error)")
+        }
     }
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // Een hele simpele "Hello World" voor de zekerheid
+            Text("App gestart")
         }
     }
 }
